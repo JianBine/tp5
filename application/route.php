c1a50c2bbe5c5c41ae277a42fe71fbf6893b3194
root <root@localhost.localdomain>
2018-05-13 18:28:49 +0800

    php think make:controller index/Warn

--- application/route.php
+++ application/route.php
@@ -17,9 +17,5 @@ return [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
-    // '[hello]'     => [
-    //     ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-    //     ':name' => ['index/hello', ['method' => 'post']],
-    // ],
-
+    
 ];
