b3bf04764ec4466891fb22051c96aa18ee5b44d6
jianbine <369932775@qq.com>
2018-05-27 04:14:18 +0800

    add base page template

---
+++ application/index/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+class Index extends Controller
+{
+    /**
+     * 管理页主页
+     */
+    public function index()
+    {
+        $title = 'money';
+        $this->view->assign('title',$title);
+        return $this->fetch('Index/index');
+    }
+
+}
---
+++ application/index/controller/index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+class Index extends Controller
+{
+    /**
+     * 管理页主页
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+//        $title = 'money';
+//        $this->view->assign('title',$title);
+//        return $this->view->fetch();
+    }
+
+}
