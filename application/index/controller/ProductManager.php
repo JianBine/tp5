d6e1db61cd5d4e5484a3d51910a401ebfc45be14
jianbine <369932775@qq.com>
2018-06-03 13:49:36 +0800

    base function

--- application/index/controller/ProductManager.php
+++ application/index/controller/ProductManager.php
@@ -7,6 +7,7 @@ use think\Request;
 use app\index\model\Order;
 use app\index\model\OrderDetail;
 use app\index\model\Product;
+use app\index\model\Employee;
 class ProductManager extends Controller
 {
     /**
@@ -16,12 +17,15 @@ class ProductManager extends Controller
      */
     public function index()
     {
+        //获取员工列表
+        $employeeInfos = Employee::all();
         //获取订单列表
         $orderInfos = Order::all();
         //分页显示订单列表
-        $list = OrderDetail::paginate(6);
+        $list = Product::paginate(6);
         // 获取分页显示
         $page = $list->render();
+        $this->assign('employeeInfos', $employeeInfos);
         $this->assign('orderInfos', $orderInfos);
         $this->assign('list', $list);
         $this->assign('page', $page);
@@ -39,14 +43,38 @@ class ProductManager extends Controller
     }
 
     /**
-     * 保存新建的资源
+     * 保存产量信息
      *
      * @param  \think\Request  $request
-     * @return \think\Response
      */
     public function save(Request $request)
     {
-        //
+        $dataJson = $request->post('data');
+        $info = array();
+        if($dataJson){
+            $dataArray = json_decode($dataJson,true);
+            $employee_id = $dataArray['employee_id'];
+            $order_id = $dataArray['order_id'];
+            $order_detail_id = $dataArray['order_detail_id'];
+            $number = $dataArray['number'];
+            $Product = new Product();
+            $Product->employee_id = $employee_id;
+            $Product->order_id = $order_id;
+            $Product->order_detail_id = $order_detail_id;
+            $Product->number = $number;
+            $res = $Product->save();
+            if($res != 0){
+                $info['message'] = '添加成功';
+                $info['status'] = 'success';
+            }else{
+                $info['message'] = '添加失败';
+                $info['status'] = 'failure';
+            }
+        }else{
+            $info['message'] = '添加失败';
+            $info['status'] = 'failure';
+        }
+        echo json_encode($info);
     }
 
     /**
@@ -61,14 +89,38 @@ class ProductManager extends Controller
     }
 
     /**
-     * 显示编辑资源表单页.
+     * 显示产档详情.
      *
      * @param  int  $id
      * @return \think\Response
      */
     public function edit($id)
     {
-        //
+        $id = $id ? $id : -1;
+        $info = array();
+        if($id != -1){
+            $result = Product::get($id);
+            if($result){
+                //获取员工列表
+                $employeeInfos = Employee::all();
+                //获取订单列表
+                $orderInfos = Order::all();
+                $orderDetailInfos = OrderDetail::where(array('order_id'=>$result->order_id))->select();
+                $info['employeeInfos'] = $employeeInfos;
+                $info['orderInfos'] = $orderInfos;
+                $info['orderDetailInfos'] = $orderDetailInfos;
+                $info['message'] = '返回成功';
+                $info['data'] = $result;
+                $info['status'] = 'success';
+            }else{
+                $info['message'] = '返回失败';
+                $info['status'] = 'failure';
+            }
+        }else{
+            $info['message'] = '用户不存在';
+            $info['status'] = 'failure';
+        }
+        echo json_encode($info);
     }
 
     /**
@@ -80,17 +132,75 @@ class ProductManager extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $dataJson = $request->post('data');
+        $info = array();
+        if(empty($id)){
+            $info['message'] = '工序不存在';
+            $info['status'] = 'failure';
+            echo json_encode($info);
+            exit();
+        }
+        if($dataJson){
+            $dataArray = json_decode($dataJson,true);
+            $edit_employee_id = $dataArray['edit_employee_id'];
+            $edit_order_id = $dataArray['edit_order_id'];
+            $edit_order_detail_id = $dataArray['edit_order_detail_id'];
+            $edit_number = $dataArray['edit_number'];
+            $id = intval($id);
+            $Product = Product::get($id);
+            $Product->employee_id = $edit_employee_id;
+            $Product->order_id = $edit_order_id;
+            $Product->order_detail_id = $edit_order_detail_id;
+            $Product->number = $edit_number;
+            $res = $Product->save();
+            if($res != 0){
+                $info['message'] = '编辑成功';
+                $info['status'] = 'success';
+            }else{
+                $info['message'] = '编辑失败';
+                $info['status'] = 'failure';
+            }
+        }else{
+            $info['message'] = '编辑失败';
+            $info['status'] = 'failure';
+        }
+        echo json_encode($info);
     }
 
     /**
      * 删除指定资源
      *
      * @param  int  $id
-     * @return \think\Response
      */
     public function delete($id)
     {
-        //
+        $id = $id ? $id : -1;
+        $info = array();
+        if($id != -1){
+            $result = Product::destroy($id);
+            if($result != 0){
+                $info['message'] = '删除成功';
+                $info['status'] = 'success';
+            }else{
+                $info['message'] = '删除失败';
+                $info['status'] = 'failure';
+            }
+        }else{
+            $info['message'] = '删除失败';
+            $info['status'] = 'failure';
+        }
+        echo json_encode($info);
+    }
+
+    /**
+     * 获取对应订单对应的工序
+     */
+    public function getOrderDetail(){
+        $id = input('id');
+        $infos = OrderDetail::where(array('order_id'=>$id))->select();
+        $info = array();
+        $info['status'] = 'success';
+        $info['data'] = $infos;
+        echo json_encode($info);
     }
 }
